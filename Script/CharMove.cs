816b96feb024caa29b5d1c8d922c8fec7053e62e
HongSangJoon <61660260+DogGuyMan@users.noreply.github.com>
2022-04-14 15:40:05 +0900

    Update CharMove.cs

--- Script/CharMove.cs
+++ Script/CharMove.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CharMove : MonoBehaviour
 { 
 	public bool IsJump = false; //���߿� �ִ���?
-	bool IsDash;
-	public bool DashCool = true;
+		bool IsDash;
+	public bool DashCool = true; //대쉬 Is on 쿨타임
 
 	public float maxSpeed = 5;		//ĳ���� �ִ뽺�ǵ�
 	public float maxJump = 2f;		//���� �ִ� ����
@@ -15,18 +15,27 @@ public class CharMove : MonoBehaviour
 
 	public float MoveSpeed = 0.5f;
 
-	public float DashSpeed;
-	public float DashTimer;
-	public float DashCoolTime;
+	public float DashSpeed;		//인스펙터에서 입력
+	public float DashTimer;		//인스펙터에서 입력
+	public float DashCoolTime;	//인스펙터에서 입력
 
 	float CurrentDashTimer;
 	float DashDirection = 1;
 	float CurrentDashCoolTime;
 	bool DEBUG = false;
 	Rigidbody2D rigid;
-	GameObject Character;
+	GameObject Character;		//스크립트 적용 대상은 Character
 	float CharY;            //�����ϱ� �� ����
-
+	
+	/*************************************************************
+	* 캐릭터의 음직임은 다음과 같다
+	* 1. 방향키 
+		A : 좌측 
+		D : 우측
+		(space) : 점프
+	* 2. 마우스 
+		우클릭 : 
+	*************************************************************/
     void Awake()
 	{
 		Character = gameObject;
@@ -36,24 +45,14 @@ public class CharMove : MonoBehaviour
     void Update()   //�ܹ����� ������ Update
 	{
 		if(DEBUG) {Debug.Log(CharY);}
-		if (Input.GetKeyUp(KeyCode.A))
-        {
-			rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.25f, rigid.velocity.y);
-		}
-
-		if (Input.GetKeyUp(KeyCode.D))
-		{
-			rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.25f, rigid.velocity.y);
-		}
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
+		if (Input.GetKeyUp(KeyCode.A)){rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.25f, rigid.velocity.y);}
+		if (Input.GetKeyUp(KeyCode.D)){rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.25f, rigid.velocity.y);}
+		
+		if (Input.GetKeyDown(KeyCode.Space)){
 			CharY = transform.position.y + maxJump;
-			if (JumpCount == 0)
-            {
-				IsJump = false;
-            }
-            else
-            {
+			if (JumpCount == 0){IsJump = false;}
+			else
+			{
 				rigid.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
 				IsJump = true;
 				JumpCount--;
@@ -64,6 +63,7 @@ public class CharMove : MonoBehaviour
 		{
 			DashCool = false;
 			GameObject.Find("GameManager").SendMessage("DashCoolOff");
+			//디버깅 UI 텍스트 ON/OFF 목적
 			IsDash = true;
 			CurrentDashTimer = DashTimer;
 			CurrentDashCoolTime = DashCoolTime;
@@ -86,68 +86,33 @@ public class CharMove : MonoBehaviour
 		}
 
 		if (!DashCool)
-        {
+		{
 			CurrentDashCoolTime -= Time.deltaTime;
-
 			if (CurrentDashCoolTime <= 0)
-            {
+			{
 				DashCool = true;
 				GameObject.Find("GameManager").SendMessage("DashCoolOn");
 			}
-        }
-
-
-
-		if (Input.GetKeyDown(KeyCode.S))
-		{
-			rigid.AddForce(Vector2.down * 0.5f, ForceMode2D.Impulse);
 		}
 
 	}
-
-    void FixedUpdate()	//���������� ������ ������ FixedUpdate (1�ʿ� 50��)
+	
+	void FixedUpdate()	//���������� ������ ������ FixedUpdate (1�ʿ� 50��)
 	{
-		if (Input.GetKey(KeyCode.A))
-		{
-			rigid.AddForce(Vector2.left * MoveSpeed, ForceMode2D.Impulse);
-			DashDirection = -1;
-		}
+		if (Input.GetKey(KeyCode.A)){rigid.AddForce(Vector2.left * MoveSpeed, ForceMode2D.Impulse); DashDirection = -1;}
+		if (Input.GetKey(KeyCode.D)){rigid.AddForce(Vector2.right * MoveSpeed, ForceMode2D.Impulse);DashDirection = 1; }
 
-		if (Input.GetKey(KeyCode.D))
-		{
-			rigid.AddForce(Vector2.right * MoveSpeed, ForceMode2D.Impulse);
-			DashDirection = 1;
-		}
-
-		if (Input.mousePosition.x < Screen.width / 2)
-        {
-			//DashDirection = -1;
-        }
-        else
-        {
-			//DashDirection = 1;
-        }
-
-		if (Input.GetKey(KeyCode.Space))
-		{
-			if (IsJump)
-            {
+		if (Input.GetKey(KeyCode.Space)) {
+			if (IsJump) {
 				rigid.AddForce(Vector2.up * JumpSpeed, ForceMode2D.Impulse);
-
-				if (transform.position.y >= CharY)
-				{
-					IsJump = false;
-				}
+				if (transform.position.y >= CharY){IsJump = false;}
 			}
 		}
 
 		if (!IsDash)
-        {
-			if (rigid.velocity.x > maxSpeed)
-				rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-
-			else if (rigid.velocity.x < maxSpeed * (-1))
-				rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+		{
+			if (rigid.velocity.x > maxSpeed) {rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y)};
+			else if (rigid.velocity.x < maxSpeed * (-1)){rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y)};
 		}
 	}
 }
