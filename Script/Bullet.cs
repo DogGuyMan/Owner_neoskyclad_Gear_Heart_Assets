e2a67347a2068bcafbc7a67d634db8c4fc9d270a
HongSangJoon <61660260+DogGuyMan@users.noreply.github.com>
2022-04-14 15:21:48 +0900

    Update Bullet.cs

--- Script/Bullet.cs
+++ Script/Bullet.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    float speed = 10f;
-    float lifeTime = 3;
+    float speed = 10f; //총알 스피드
+    float lifeTime = 3; //날아가는동안 N초가 지나면 삭제
     Vector2 target;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-            Destroy(gameObject);
+            Destroy(gameObject); //총알 삭제
         }
 
         if (collision.gameObject.name == "Character")
         {
             GameObject.Find("GameManager").SendMessage("GetHit");
-            Destroy(gameObject);
+            Destroy(gameObject); //총알 삭제
         }
     }
 
@@ -26,11 +26,13 @@ public class Bullet : MonoBehaviour
     void Start()
     {
         Destroy(gameObject, lifeTime);
+        //비동기적 life타임 지나면 삭제
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.up * speed * Time.deltaTime);
+        //진행 방향으로 나아가기
     }
 }
